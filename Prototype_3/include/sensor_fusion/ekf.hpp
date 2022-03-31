7f5656c0f51f9d3bdd398d3fc43cd4c9c4dcf039
CraigNewlands <craigarnewlands@gmail.com>
2022-03-31 16:22:24 +0100

    Prototype 3 implementation

---
+++ Prototype_3/include/sensor_fusion/ekf.hpp
@@ -0,0 +1,12 @@
+namespace sensor_fusion {
+
+Ekf::Ekf() : rclcpp::Node("ekf") {
+
+}
+
+void Ekf::correct(const Measurement & measurement) {
+    
+}
+
+
+}
---
+++ Prototype_3/src/ekf.cpp
@@ -0,0 +1,12 @@
+#include <sensor_fusion/ekf.hpp>
+
+#include <cstdio>
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world sensor_fusion package\n");
+  return 0;
+}
