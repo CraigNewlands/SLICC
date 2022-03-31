82dd665a46ca2ef57cd79133f13f2018932b109c
CraigNewlands <craigarnewlands@gmail.com>
2022-03-31 15:41:24 +0100

    Prototype 2 implementation

---
+++ Prototype 2/python_ekf/ekf_data.py
@@ -0,0 +1,159 @@
+import rclpy
+from rclpy.qos import QoSPresetProfiles, QoSProfile
+from rclpy.node import Node
+from datetime import datetime
+import time
+import message_filters
+import numpy as np
+from numpy.linalg import inv
+import math
+import jax
+from jax import jacfwd
+from jax.numpy import asarray, sin
+from std_msgs.msg import String, Float32MultiArray
+from sensor_msgs.msg import Imu
+from eufs_msgs.msg import WheelSpeedsStamped
+from geometry_msgs.msg import TwistWithCovarianceStamped
+
+
+class EKF(Node):
+
+    def __init__(self):
+        super().__init__('ekf_publisher')
+        self.publisher_ = self.create_publisher(TwistWithCovarianceStamped, 'ekf_data', 10)
+
+        # BAG FILE SUBSCRIBERS
+        self.wheel_data_sub = message_filters.Subscriber(self, WheelSpeedsStamped, 'ros_can/wheel_speeds')
+        self.sbg_imu_data_sub = message_filters.Subscriber(self, Imu, '/imu/data')
+        self.zed_imu_data_sub = message_filters.Subscriber(self, Imu, '/camera/imu/data')
+
+
+        # SIMULATION SUBSCRIBERS
+        # self.zed_imu_data_sub = message_filters.Subscriber(self, Imu, 'imu', qos_profile=QoSPresetProfiles.SENSOR_DATA.value)
+        # self.wheel_data_sub = message_filters.Subscriber(self, WheelSpeedsStamped, 'ros_can/wheel_speeds')
+        # self.gps_velocity_data_sub = message_filters.Subscriber(self, TwistWithCovarianceStamped, 'fix_velocity')
+        # self.sbg_imu_data_sub = message_filters.Subscriber(self, Imu, '/imu/data')
+
+        time_synchronizer = message_filters.ApproximateTimeSynchronizer([self.wheel_data_sub, self.sbg_imu_data_sub, self.zed_imu_data_sub], queue_size=10, slop=1)
+        # time_synchronizer = message_filters.ApproximateTimeSynchronizer([self.wheel_data_sub, self.zed_imu_data_sub], queue_size=10, slop=1)
+        time_synchronizer.registerCallback(self.callback)
+
+        self.previous_time = time.time()
+
+        # Initial state vector [vel_x, vel_y, yaw_rate, acc_x, acc_y]
+        self.x = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
+
+        # Covariance matrix for the uncertainty in the initial state
+        self.P = np.array([[500, 0, 0, 0, 0],
+                           [0, 500, 0, 0, 0],
+                           [0, 0, 500, 0, 0],
+                           [0, 0, 0, 500, 0],
+                           [0, 0, 0, 0, 500]])
+
+        self.wheelbase_length = 1.53
+
+        #TODO figure out how to compute this (hint: look at robot_localization)
+        self.process_noise_covariance = np.array([[1, 0, 0, 0, 0],
+                                                  [0, 1, 0, 0, 0],
+                                                  [0, 0, 1, 0, 0],
+                                                  [0, 0, 0, 1, 0],
+                                                  [0, 0, 0, 0, 1]])
+
+        #TODO figure out how to compute this 
+        self.measurement_noise_covariance = np.array([[1, 0, 0],
+                                                      [0, 1, 0],
+                                                      [0, 0, 1]])
+
+                                        
+
+    def state_prediction(self, previous_state, wheel_control_data, dt):
+
+        return asarray([previous_state[0] + previous_state[3] * dt,
+                        previous_state[1] + previous_state[4] * dt,
+                        (previous_state[0] * sin(wheel_control_data[0])) / self.wheelbase_length,
+                        previous_state[3],
+                        previous_state[4]])
+
+
+    def measurement_prediction(self, predicted_state):
+
+        predicted_measurement = asarray([predicted_state[2], predicted_state[3], predicted_state[4]])
+
+        return predicted_measurement
+
+
+    def callback(self, wheel_data, sbg_data, zed_data):
+    # def callback(self, wheel_data, zed_data):
+        wheel_control_data = np.array([wheel_data.speeds.steering])
+        sbg_imu_data = np.array([sbg_data.angular_velocity.z, sbg_data.linear_acceleration.x, sbg_data.linear_acceleration.y])
+        zed_imu_data = np.array([zed_data.angular_velocity.z, zed_data.linear_acceleration.x, zed_data.linear_acceleration.y])
+
+        # gps_velocity_data = np.array([gps_data.twist.twist.linear.x, gps_data.twist.twist.linear.y, gps_data.twist.twist.linear.z])
+        # print("Zed Linear Acceleration = " + str(zed_linear_acc_data))
+        # print("DGPS Linear Velocity = " + str(gps_velocity_data))
+
+        current_time = time.time()
+        dt = time.time() - self.previous_time
+        self.previous_time = current_time
+
+        G = jacfwd(lambda steering: self.state_prediction(self.x, steering, dt), 0)(wheel_control_data)
+
+        # Prediction step - control data (steering angle)
+        self.x = self.state_prediction(self.x, wheel_control_data, dt)
+        self.P = G.transpose().dot(self.P).dot(G) + self.process_noise_covariance
+
+
+        # Correction step - SBG IMU data
+        h = self.measurement_prediction(self.x)
+        H = jacfwd(lambda x: self.measurement_prediction(x))(self.x)
+
+        K = self.P.dot(H.transpose()).dot(inv(H.dot(self.P).dot(H.transpose()) + self.measurement_noise_covariance))
+
+        self.x = self.x + K.dot(sbg_imu_data - h)
+
+        I = np.identity(5)
+        self.P = (I - K.dot(H)).dot(self.P)
+
+
+        # Correction step - ZED IMU data
+        h = self.measurement_prediction(self.x)
+        H = jacfwd(lambda x: self.measurement_prediction(x))(self.x)
+
+        K = self.P.dot(H.transpose()).dot(inv(H.dot(self.P).dot(H.transpose()) + self.measurement_noise_covariance))
+
+        self.x = self.x + K.dot(zed_imu_data - h)
+
+        I = np.identity(5)
+        self.P = (I - K.dot(H)).dot(self.P)
+
+
+        print(self.x)
+
+        msg = TwistWithCovarianceStamped()
+        msg.header.frame_id = 'base_footprint'
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.twist.twist.linear.x = float(self.x[0])
+        msg.twist.twist.linear.y = float(self.x[1])
+        msg.twist.twist.angular.z = float(self.x[2])
+        msg.twist.covariance[0] = float(self.P[0][0])
+        msg.twist.covariance[7] = float(self.P[1][1])
+        msg.twist.covariance[35] = float(self.P[2][2])
+        self.publisher_.publish(msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    ekf = EKF()
+
+    rclpy.spin(ekf)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    ekf.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ Prototype_1/README.md
@@ -0,0 +1,20 @@
+# Prototype 1 - Velocity Kalman Filter
+
+This was the first prototype I created for my SLICC project and it generates its own wheel speed data, SBG data and ZED camera data.
+
+## How to run
+If you want to recompile the code then follow all of these steps, alternatively if you just want to run the compiled code go to step 5.
+1. Open the SLICC folder in VS code
+2. Install eigen from this guide https://eigen.tuxfamily.org/dox/GettingStarted.html 
+3. Add the location of the eigen folder to the tasks.json file
+4. Open the velocityKalmanFilter.cpp file and press **ctrl+shift+b** to build
+5. Run **./velocityKalmanFilter** in the terminal and the graphs should appear one at a time
+
+## Graphs
+
+The three graphs showing the kalman filter at work can also be found in this folder under the names:
+- x_velocity.png
+- y_velocity.png
+- yaw_velocity.png
+
+This is also a useful guide if the above does not work: https://code.visualstudio.com/docs/cpp/config-linux
